7d84e329877b3e9d5ed6568775bfe59b48f13b67
Ehsan-saradar <ehsan.saradar@gmail.com>
2022-02-02 22:25:16 +0330

    Remove ack and timeout handlers for ibcCreatePool

--- x/intergamm/keeper/ibc_create_pool.go
+++ x/intergamm/keeper/ibc_create_pool.go
@@ -1,7 +1,6 @@
 package keeper
 
 import (
-	"errors"
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -28,40 +27,4 @@ func (k Keeper) OnRecvIbcCreatePoolPacket(ctx sdk.Context, packet channeltypes.P
 	// TODO: emit events related to creating a pool
 
 	return packetAck, nil
-}
-
-// OnAcknowledgementIbcCreatePoolPacket responds to the the success or failure of a packet
-// acknowledgement written on the receiving chain.
-func (k Keeper) OnAcknowledgementIbcCreatePoolPacket(ctx sdk.Context, packet channeltypes.Packet, data types.IbcCreatePoolPacketData, ack channeltypes.Acknowledgement) error {
-	switch dispatchedAck := ack.Response.(type) {
-	case *channeltypes.Acknowledgement_Error:
-
-		// TODO: failed acknowledgement logic
-		_ = dispatchedAck.Error
-
-		return nil
-	case *channeltypes.Acknowledgement_Result:
-		// Decode the packet acknowledgment
-		var packetAck types.IbcCreatePoolPacketAck
-
-		if err := types.ModuleCdc.UnmarshalJSON(dispatchedAck.Result, &packetAck); err != nil {
-			// The counter-party module doesn't implement the correct acknowledgment format
-			return errors.New("cannot unmarshal acknowledgment")
-		}
-
-		// TODO: successful acknowledgement logic
-
-		return nil
-	default:
-		// The counter-party module doesn't implement the correct acknowledgment format
-		return errors.New("invalid acknowledgment format")
-	}
-}
-
-// OnTimeoutIbcCreatePoolPacket responds to the case where a packet has not been transmitted because of a timeout
-func (k Keeper) OnTimeoutIbcCreatePoolPacket(ctx sdk.Context, packet channeltypes.Packet, data types.IbcCreatePoolPacketData) error {
-
-	// TODO: packet timeout logic
-
-	return nil
 }
